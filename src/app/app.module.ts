970b5d129f5c9a4309d70e674ce3d67753b54b98
Piotr <piotr.kaczyski9@wp.pl>
2024-02-19 17:54:33 +0100

    formularz wysłania wiadomosci

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 import {CoreModule} from './core/core.module';
 import {PagesModule} from './pages/pages.module';
